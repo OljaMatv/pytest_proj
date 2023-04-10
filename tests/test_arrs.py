37a2059213bb37c12dc365ae2d320e02f7f8b3ff
olja <matvejevaolga@gmail.com>
2023-04-10 21:24:38 +0300

    test3

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,12 +4,9 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([1], 0, "test") == 1
-# if __name__ == '__main__':
-#     test_get()
+
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-# if __name__ == '__main__':
-#     test_get()
